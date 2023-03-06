887eedcd507febba4c47af247b84c42081f7c044
Xhemanuelv <xhemanuelv@gmail.com>
2023-03-06 16:42:07 -0300

    Agregado progreso ejercicios clase 1

---
+++ Clase1/Clase1ArgProgr/src/clase1argprogr/Clase1ArgProgr.java
@@ -0,0 +1,17 @@
+package clase1argprogr;
+
+import clase1argprogr.Servicio.Ejercicios;
+
+/**
+ *
+ * @author Emanuel Villarin
+ */
+public class Clase1ArgProgr {
+
+    public static void main(String[] args) {
+        Ejercicios ej = new Ejercicios();
+
+        ej.ejercicio1();
+    }
+
+}
